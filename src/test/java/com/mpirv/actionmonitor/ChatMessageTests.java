b5987f163a19960fc45478e0ee31f9246a95feb8
Marius <lord_mariusstar@yahoo.com>
2022-06-07 22:44:14 +0300

    6th Commit

--- src/test/java/com/mpirv/actionmonitor/ChatMessageTests.java
+++ src/test/java/com/mpirv/actionmonitor/ChatMessageTests.java
@@ -26,7 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration(classes = ActionMonitorApplication.class)
-public class MessageIntegrationTests {
+public class ChatMessageTests {
 	@Autowired
 	private WebApplicationContext wac;
 	private MockMvc mockMvc;
